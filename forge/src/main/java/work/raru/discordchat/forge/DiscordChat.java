b19050fbadce32f16fa09236828cf95c674d3f78
ozraru <34647405+ozraru@users.noreply.github.com>
2022-11-19 23:55:02 +0900

    Fix forge one-sided stting

--- forge/src/main/java/work/raru/discordchat/forge/DiscordChat.java
+++ forge/src/main/java/work/raru/discordchat/forge/DiscordChat.java
@@ -3,6 +3,7 @@ package work.raru.discordchat.forge;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -10,11 +11,14 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.server.permission.DefaultPermissionLevel;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.server.permission.PermissionAPI;
 import work.raru.discordchat.common.Main;
 import work.raru.discordchat.common.Permissions;
 
 import java.sql.SQLException;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("discordchat")
@@ -22,6 +26,10 @@ public class DiscordChat {
     ForgePlatform platform;
 
     public DiscordChat() throws SQLException, ClassNotFoundException {
+        // This is One-Sided Mod
+        // https://docs.minecraftforge.net/en/1.16.x/concepts/sides/#writing-one-sided-mods
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        
         platform = new ForgePlatform();
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, platform.config.configSpec);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::start);
